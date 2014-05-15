5492502e6709fe7778b02b9a5b2cb834e45fb9b7
David Molineus <mail@netzmacht.de>
2014-05-15 16:25:03 +0200

    add templates to form module

---
+++ module/config/autoload.php
@@ -0,0 +1,7 @@
+<?php
+
+TemplateLoader::addFiles(array(
+	'form_checkbox_generate'      => 'system/modules/bootstrap-form',
+	'form_radio_generate'         => 'system/modules/bootstrap-form',
+	'formhelper_layout_bootstrap' => 'system/modules/bootstrap-form',
+));
