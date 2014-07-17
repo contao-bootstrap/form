065f90de0208c6cfbc2ac55f77996c1718cbb75c
David Molineus <mail@netzmacht.de>
2014-07-17 15:51:58 +0200

    add layout template without blocks

--- module/config/autoload.php
+++ module/config/autoload.php
@@ -2,7 +2,14 @@
 
 TemplateLoader::addFiles(array(
 	'form'         				    => 'system/modules/bootstrap-form/templates',
-	'formhelper_layout_bootstrap'   => 'system/modules/bootstrap-form/templates',
 	'formhelper_element_radios'     => 'system/modules/bootstrap-form/templates',
 	'formhelper_element_checkboxes' => 'system/modules/bootstrap-form/templates',
 ));
+
+
+if(version_compare(VERSION, '3.3', '<')) {
+	TemplateLoader::addFile('formhelper_layout_bootstrap', 'system/modules/bootstrap-form/templates/3.2');
+}
+else {
+	TemplateLoader::addFile('formhelper_layout_bootstrap', 'system/modules/bootstrap-form/templates/3.3');
+}
