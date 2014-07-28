97aab43df110b334e34e8ed3ab5db22034f11472
David Molineus <mail@netzmacht.de>
2014-07-28 11:24:50 +0200

    rework templates to work with Contaos very limited templates editor

--- module/config/autoload.php
+++ module/config/autoload.php
@@ -1,15 +1,10 @@
 <?php
 
 TemplateLoader::addFiles(array(
-	'form'         				    => 'system/modules/bootstrap-form/templates',
-	'formhelper_element_radios'     => 'system/modules/bootstrap-form/templates',
-	'formhelper_element_checkboxes' => 'system/modules/bootstrap-form/templates',
+	'form'         				     => 'system/modules/bootstrap-form/templates',
+	'formhelper_element_radios'      => 'system/modules/bootstrap-form/templates',
+	'formhelper_element_checkboxes'  => 'system/modules/bootstrap-form/templates',
+	'formhelper_layout_bootstrap'    => 'system/modules/bootstrap-form/templates',
+	'formhelper_layout_bootstrap_32' => 'system/modules/bootstrap-form/templates',
+	'formhelper_layout_bootstrap_33' => 'system/modules/bootstrap-form/templates',
 ));
-
-
-if(version_compare(VERSION, '3.3', '<')) {
-	TemplateLoader::addFile('formhelper_layout_bootstrap', 'system/modules/bootstrap-form/templates/3.2');
-}
-else {
-	TemplateLoader::addFile('formhelper_layout_bootstrap', 'system/modules/bootstrap-form/templates/3.3');
-}
