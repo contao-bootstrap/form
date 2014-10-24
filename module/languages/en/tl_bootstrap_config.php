ea7281096e87dea50bcf4d437f4cd5966a4455fe
David Molineus <mail@netzmacht.de>
2014-10-24 15:46:00 +0200

    add translation of widget type

---
+++ module/languages/en/tl_bootstrap_config.php
@@ -0,0 +1,16 @@
+<?php
+
+$GLOBALS['TL_LANG']['bootstrap_config_types']['form_widget'] = 'Form Widget';
+
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_widget_control'][0]       = 'Form-Control';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_widget_control'][1]       = 'Add the <em>form-control</em> css class.';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_widget_label'][0]         = 'Label';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_widget_label'][1]         = 'Element has a label.';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_widget_input_group'][0]   = 'Input-Group';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_widget_input_group'][1]   = 'Element can be wrapped with an input group.';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_widget_styled_select'][0] = 'Styled-Select';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_widget_styled_select'][1] = 'Use Styled-Select to style the select list.';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_widget_styled_upload'][0] = 'Styled-Upload';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_widget_styled_upload'][1] = 'Style the file upload field';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_widget_modal_footer'][0]  = 'Footer of modal';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_widget_modal_footer'][1]  = 'If the form is displayed in a modal window move this widget to the footer.';
