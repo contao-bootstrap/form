7ee7eaaffca0b0cd3339968b806bd23cb0fda4ee
David Molineus <david.molineus@netzmacht.de>
2015-02-06 12:35:36 +0100

    Improve horizontal form switching label (see #8).

---
+++ module/languages/de/tl_form.php
@@ -0,0 +1,4 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_form']['tableless'][0] = 'Kein horizontales Formular';
+$GLOBALS['TL_LANG']['tl_form']['tableless'][1] = 'Das Formular nicht als horizontales Formular (Bootstrap) / HTML-Tabellen (Contao) darstellen.';
---
+++ module/languages/en/tl_form.php
@@ -0,0 +1,4 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_form']['tableless'][0] = 'Disable horizontal form';
+$GLOBALS['TL_LANG']['tl_form']['tableless'][1] = 'Do not display form as horizontal form (Bootstrap) / with HTML tables (Contao).';
