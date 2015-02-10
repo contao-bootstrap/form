0d5c534311a8ec9c1bae6bcd4c935f72cbc922ed
David Molineus <david.molineus@netzmacht.de>
2015-02-10 18:48:56 +0100

    Indent by space, not by tabs.

--- module/config/autoload.php
+++ module/config/autoload.php
@@ -8,10 +8,10 @@
  */
 
 TemplateLoader::addFiles(array(
-	'form'         				     => 'system/modules/bootstrap-form/templates',
-	'formhelper_element_radios'      => 'system/modules/bootstrap-form/templates',
-	'formhelper_element_checkboxes'  => 'system/modules/bootstrap-form/templates',
-	'formhelper_layout_bootstrap'    => 'system/modules/bootstrap-form/templates',
-	'formhelper_layout_bootstrap_32' => 'system/modules/bootstrap-form/templates',
-	'formhelper_layout_bootstrap_33' => 'system/modules/bootstrap-form/templates',
+    'form'                           => 'system/modules/bootstrap-form/templates',
+    'formhelper_element_radios'      => 'system/modules/bootstrap-form/templates',
+    'formhelper_element_checkboxes'  => 'system/modules/bootstrap-form/templates',
+    'formhelper_layout_bootstrap'    => 'system/modules/bootstrap-form/templates',
+    'formhelper_layout_bootstrap_32' => 'system/modules/bootstrap-form/templates',
+    'formhelper_layout_bootstrap_33' => 'system/modules/bootstrap-form/templates',
 ));
--- module/dca/tl_form_field.php
+++ module/dca/tl_form_field.php
@@ -16,18 +16,18 @@ $GLOBALS['TL_DCA']['tl_form_field']['palettes']['button'] = $GLOBALS['TL_DCA']['
 
 
 foreach(Bootstrap::getConfigVar('form.widgets', array()) as $widget => $config) {
-	if(isset($config['input-group']) && $config['input-group']) {
-		\MetaPalettes::appendAfter('tl_form_field', $widget, 'fconfig', array
-		(
-			'icon' => array(':hide', 'bootstrap_addIcon'),
-			'unit' => array(':hide', 'bootstrap_addUnit'),
-		));
-	}
+    if(isset($config['input-group']) && $config['input-group']) {
+        \MetaPalettes::appendAfter('tl_form_field', $widget, 'fconfig', array
+        (
+            'icon' => array(':hide', 'bootstrap_addIcon'),
+            'unit' => array(':hide', 'bootstrap_addUnit'),
+        ));
+    }
 }
 
 \MetaPalettes::appendAfter('tl_form_field', 'button', 'type', array
 (
-	'icon' => array('bootstrap_addIcon'),
+    'icon' => array('bootstrap_addIcon'),
 ));
 
 // append inlineStyle option to radio and checkbox
@@ -49,104 +49,104 @@ $GLOBALS['TL_DCA']['tl_form_field']['metasubpalettes']['addSubmit'] = array('sla
  * fields
  */
 $GLOBALS['TL_DCA']['tl_form_field']['fields']['type']['options_callback'] = function () {
-	return array_keys($GLOBALS['TL_FFL']);
+    return array_keys($GLOBALS['TL_FFL']);
 };
 
 $GLOBALS['TL_DCA']['tl_form_field']['fields']['bootstrap_addIcon'] = array
 (
-	'label'                   => &$GLOBALS['TL_LANG']['tl_form_field']['bootstrap_addIcon'],
-	'exclude'                 => true,
-	'inputType'               => 'checkbox',
-	'eval'                    => array('submitOnChange' => true),
-	'sql'                     => "char(1) NOT NULL default ''"
+    'label'                   => &$GLOBALS['TL_LANG']['tl_form_field']['bootstrap_addIcon'],
+    'exclude'                 => true,
+    'inputType'               => 'checkbox',
+    'eval'                    => array('submitOnChange' => true),
+    'sql'                     => "char(1) NOT NULL default ''"
 );
 
 $GLOBALS['TL_DCA']['tl_form_field']['fields']['bootstrap_icon'] = array
 (
-	'label'                   => &$GLOBALS['TL_LANG']['tl_form_field']['bootstrap_icon'],
-	'exclude'                 => true,
-	'inputType'               => 'icon',
-	'options'                 => Bootstrap::getIconSet()->getIcons(),
-	'eval'                    => array('tl_class' => 'w50', 'iconTemplate' => Bootstrap::getIconSet()->getTemplate()),
-	'sql'                     => "varchar(32) NOT NULL default ''",
+    'label'                   => &$GLOBALS['TL_LANG']['tl_form_field']['bootstrap_icon'],
+    'exclude'                 => true,
+    'inputType'               => 'icon',
+    'options'                 => Bootstrap::getIconSet()->getIcons(),
+    'eval'                    => array('tl_class' => 'w50', 'iconTemplate' => Bootstrap::getIconSet()->getTemplate()),
+    'sql'                     => "varchar(32) NOT NULL default ''",
 );
 
 $GLOBALS['TL_DCA']['tl_form_field']['fields']['bootstrap_iconPosition'] = array
 (
-	'label'                   => &$GLOBALS['TL_LANG']['tl_form_field']['bootstrap_iconPosition'],
-	'exclude'                 => true,
-	'inputType'               => 'select',
-	'options'                 => array('left', 'right'),
-	'reference'               => &$GLOBALS['TL_LANG']['tl_form_field'],
-	'eval'                    => array('tl_class' => 'w50'),
-	'sql'                     => "varchar(32) NOT NULL default ''"
+    'label'                   => &$GLOBALS['TL_LANG']['tl_form_field']['bootstrap_iconPosition'],
+    'exclude'                 => true,
+    'inputType'               => 'select',
+    'options'                 => array('left', 'right'),
+    'reference'               => &$GLOBALS['TL_LANG']['tl_form_field'],
+    'eval'                    => array('tl_class' => 'w50'),
+    'sql'                     => "varchar(32) NOT NULL default ''"
 );
 
 $GLOBALS['TL_DCA']['tl_form_field']['fields']['bootstrap_addUnit'] = array
 (
-	'label'                   => &$GLOBALS['TL_LANG']['tl_form_field']['bootstrap_addUnit'],
-	'exclude'                 => true,
-	'inputType'               => 'checkbox',
-	'eval'                    => array('submitOnChange' => true, 'tl_class' => 'clr'),
-	'sql'                     => "char(1) NOT NULL default ''"
+    'label'                   => &$GLOBALS['TL_LANG']['tl_form_field']['bootstrap_addUnit'],
+    'exclude'                 => true,
+    'inputType'               => 'checkbox',
+    'eval'                    => array('submitOnChange' => true, 'tl_class' => 'clr'),
+    'sql'                     => "char(1) NOT NULL default ''"
 );
 
 $GLOBALS['TL_DCA']['tl_form_field']['fields']['bootstrap_unit'] = array
 (
-	'label'                   => &$GLOBALS['TL_LANG']['tl_form_field']['bootstrap_unit'],
-	'exclude'                 => true,
-	'inputType'               => 'text',
-	'eval'                    => array('tl_class' => 'w50'),
-	'sql'                     => "varchar(32) NOT NULL default ''"
+    'label'                   => &$GLOBALS['TL_LANG']['tl_form_field']['bootstrap_unit'],
+    'exclude'                 => true,
+    'inputType'               => 'text',
+    'eval'                    => array('tl_class' => 'w50'),
+    'sql'                     => "varchar(32) NOT NULL default ''"
 );
 
 $GLOBALS['TL_DCA']['tl_form_field']['fields']['bootstrap_unitPosition'] = array
 (
-	'label'                   => &$GLOBALS['TL_LANG']['tl_form_field']['bootstrap_unitPosition'],
-	'exclude'                 => true,
-	'inputType'               => 'select',
-	'options'                 => array('left', 'right'),
-	'reference'               => &$GLOBALS['TL_LANG']['tl_form_field'],
-	'eval'                    => array('tl_class' => 'w50'),
-	'sql'                     => "varchar(32) NOT NULL default ''"
+    'label'                   => &$GLOBALS['TL_LANG']['tl_form_field']['bootstrap_unitPosition'],
+    'exclude'                 => true,
+    'inputType'               => 'select',
+    'options'                 => array('left', 'right'),
+    'reference'               => &$GLOBALS['TL_LANG']['tl_form_field'],
+    'eval'                    => array('tl_class' => 'w50'),
+    'sql'                     => "varchar(32) NOT NULL default ''"
 );
 
 $GLOBALS['TL_DCA']['tl_form_field']['fields']['bootstrap_inlineStyle'] = array
 (
-	'label'                   => &$GLOBALS['TL_LANG']['tl_form_field']['bootstrap_inlineStyle'],
-	'exclude'                 => true,
-	'inputType'               => 'checkbox',
-	'eval'                    => array('tl_class' => 'w50'),
-	'sql'                     => "char(1) NOT NULL default ''"
+    'label'                   => &$GLOBALS['TL_LANG']['tl_form_field']['bootstrap_inlineStyle'],
+    'exclude'                 => true,
+    'inputType'               => 'checkbox',
+    'eval'                    => array('tl_class' => 'w50'),
+    'sql'                     => "char(1) NOT NULL default ''"
 );
 
 $GLOBALS['TL_DCA']['tl_form_field']['fields']['bootstrap_addSubmitClass'] = array
 (
-	'label'                   => &$GLOBALS['TL_LANG']['tl_form_field']['bootstrap_addSubmitClass'],
-	'exclude'                 => true,
-	'inputType'               => 'text',
-	'eval'                    => array('tl_class' => 'w50'),
-	'sql'                     => "varchar(255) NOT NULL default ''"
+    'label'                   => &$GLOBALS['TL_LANG']['tl_form_field']['bootstrap_addSubmitClass'],
+    'exclude'                 => true,
+    'inputType'               => 'text',
+    'eval'                    => array('tl_class' => 'w50'),
+    'sql'                     => "varchar(255) NOT NULL default ''"
 );
 
 $GLOBALS['TL_DCA']['tl_form_field']['fields']['bootstrap_addSubmitIcon'] = array
 (
-	'label'                   => &$GLOBALS['TL_LANG']['tl_form_field']['bootstrap_icon'],
-	'exclude'                 => true,
-	'inputType'               => 'icon',
-	'options'                 => Bootstrap::getIconSet()->getIcons(),
-	'reference'               => &$GLOBALS['TL_LANG']['tl_content'],
-	'eval'                    => array('tl_class' => 'w50', 'includeBlankOption' => true, 'iconTemplate' => Bootstrap::getIconSet()->getTemplate()),
-	'sql'                     => "varchar(32) NOT NULL default ''"
+    'label'                   => &$GLOBALS['TL_LANG']['tl_form_field']['bootstrap_icon'],
+    'exclude'                 => true,
+    'inputType'               => 'icon',
+    'options'                 => Bootstrap::getIconSet()->getIcons(),
+    'reference'               => &$GLOBALS['TL_LANG']['tl_content'],
+    'eval'                    => array('tl_class' => 'w50', 'includeBlankOption' => true, 'iconTemplate' => Bootstrap::getIconSet()->getTemplate()),
+    'sql'                     => "varchar(32) NOT NULL default ''"
 );
 
 $GLOBALS['TL_DCA']['tl_form_field']['fields']['bootstrap_addSubmitIconPosition'] = array
 (
-	'label'                   => &$GLOBALS['TL_LANG']['tl_form_field']['bootstrap_iconPosition'],
-	'exclude'                 => true,
-	'inputType'               => 'select',
-	'options'                 => array('left', 'right'),
-	'reference'               => &$GLOBALS['TL_LANG']['tl_form_field'],
-	'eval'                    => array('tl_class' => 'w50'),
-	'sql'                     => "varchar(32) NOT NULL default ''"
+    'label'                   => &$GLOBALS['TL_LANG']['tl_form_field']['bootstrap_iconPosition'],
+    'exclude'                 => true,
+    'inputType'               => 'select',
+    'options'                 => array('left', 'right'),
+    'reference'               => &$GLOBALS['TL_LANG']['tl_form_field'],
+    'eval'                    => array('tl_class' => 'w50'),
+    'sql'                     => "varchar(32) NOT NULL default ''"
 );
