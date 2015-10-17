9e81e4c7e8955afc842f6080211de9450d09dd54
David Molineus <david.molineus@netzmacht.de>
2015-10-17 20:11:31 +0200

    Update translations.

--- module/languages/de/tl_bootstrap_config.php
+++ module/languages/de/tl_bootstrap_config.php
@@ -1,39 +1,51 @@
 <?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/contao-bootstrap/language/de/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2014-12-03T18:07:24+01:00
+ */
 
-$GLOBALS['TL_LANG']['bootstrap_config_type']['form_widget'] = 'Formular-Widget';
 
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_widget_control'][0]       = 'Form-Control';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_widget_control'][1]       = 'Fügt dem Element eine <em>form-control</em> Klasse zu.';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_widget_label'][0]         = 'Bezeichnung';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_widget_label'][1]         = 'Element besitzt ein Label-Element.';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_widget_input_group'][0]   = 'Input-Group';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_widget_input_group'][1]   = 'Element kann als Input group dargestellt werden.';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_widget_styled_select'][0] = 'Styled-Select';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_widget_styled_select'][1] = 'Styled-Select anwenden.';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_widget_styled_upload'][0] = 'Styled-Upload';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_widget_styled_upload'][1] = 'Datei-Upload Feld stylen.';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_widget_modal_footer'][0]  = 'Fußbereich des modalen Fenstern';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_widget_modal_footer'][1]  = 'Wird das Formular im modalen Fenster angezeigt, so wird das Element im Fußbereich dessen gerendet.';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_default_horizontal'][0]     = '2-spaltige Formulare standardmäßig nutzen';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_default_horizontal'][1]     = 'Diese Einstellung betrifft Formulare, die nicht aus dem Formulargenerator kommen.';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_horizontal_control'][0]     = 'Form Control Klasse';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_horizontal_control'][1]     = 'Diese Klasse wird für die Element-Spalte verwendet.';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_horizontal_label'][0]       = 'Spalte der Bezeichnung';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_horizontal_label'][1]       = 'Diese Klasse wird der Bezeichnungs-Spalte hinhzugefügt.';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_horizontal_offset'][0]      = 'Abstand Klasse';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_horizontal_offset'][1]      = 'Diese Klasse wird als Abstands-Klasse für Elemente ohne Bezeichnung hinzugefügt.';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_styled_select'][0]          = 'Styled Selects verwenden';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_styled_select'][1]          = 'Select Listen werden als Dropdown-Listen gestylt.';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_styled_select_class'][0]    = 'Styled Select Selektor';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_styled_select_class'][1]    = 'Diese Klasse dient als Selektor für das Styled-Select Javascript.';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_styled_select_style'][0]    = 'Styled Select Klasse';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_styled_select_style'][1]    = 'Diese Klasse wird zum Styling des Dropdowns hinzugefügt.';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_styled_upload'][0]          = 'Upload-Felder stylen';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_styled_upload'][1]          = 'Upload-Felder werden gestylt.';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_styled_upload_class'][0]    = 'Klasse der Upload-Schaltfläche';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_styled_upload_class'][1]    = 'Diese Klasse wird der Upload-Schaltfläche hinzugefügt.';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_styled_upload_position'][0] = 'Position der Schaltfläche';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_styled_upload_position'][1] = 'Position der Schaltfläche bestimmen.';
+$GLOBALS['TL_LANG']['bootstrap_config_type']['form']                           = 'Formular';
+$GLOBALS['TL_LANG']['bootstrap_config_type']['form_widget']                    = 'Formular-Widget';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_default_horizontal']['0']     = '2-spaltige Formulare standardmäßig nutzen';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_default_horizontal']['1']     = 'Diese Einstellung betrifft Formulare, die nicht aus dem Formulargenerator kommen.';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_horizontal_control']['0']     = 'Form Control Klasse';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_horizontal_control']['1']     = 'Diese Klasse wird für die Element-Spalte verwendet.';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_horizontal_label']['0']       = 'Spalte der Bezeichnung';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_horizontal_label']['1']       = 'Diese Klasse wird der Bezeichnungs-Spalte hinhzugefügt.';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_horizontal_offset']['0']      = 'Abstand Klasse';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_horizontal_offset']['1']      = 'Diese Klasse wird als Abstands-Klasse für Elemente ohne Bezeichnung hinzugefügt.';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_styled_select']['0']          = 'Styled Selects verwenden';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_styled_select']['1']          = 'Select Listen werden als Dropdown-Listen gestylt.';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_styled_select_class']['0']    = 'Styled Select Selektor';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_styled_select_class']['1']    = 'Diese Klasse dient als Selektor für das Styled-Select Javascript.';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_styled_select_style']['0']    = 'Styled Select Klasse';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_styled_select_style']['1']    = 'Diese Klasse wird zum Styling des Dropdowns hinzugefügt.';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_styled_upload']['0']          = 'Upload-Felder stylen';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_styled_upload']['1']          = 'Upload-Felder werden gestylt.';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_styled_upload_class']['0']    = 'Klasse der Upload-Schaltfläche';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_styled_upload_class']['1']    = 'Diese Klasse wird der Upload-Schaltfläche hinzugefügt.';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_styled_upload_position']['0'] = 'Position der Schaltfläche';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_styled_upload_position']['1'] = 'Position der Schaltfläche bestimmen.';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_widget_control']['0']         = 'Form-Control';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_widget_control']['1']         = 'Fügt dem Element eine <em>form-control</em> Klasse zu.';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_widget_input_group']['0']     = 'Input-Group';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_widget_input_group']['1']     = 'Element kann als Input group dargestellt werden.';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_widget_label']['0']           = 'Bezeichnung';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_widget_label']['1']           = 'Element besitzt ein Label-Element.';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_widget_modal_footer']['0']    = 'Fußbereich des modalen Fenstern';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_widget_modal_footer']['1']    = 'Wird das Formular im modalen Fenster angezeigt, so wird das Element im Fußbereich dessen gerendet.';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_widget_styled_select']['0']   = 'Styled-Select';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_widget_styled_select']['1']   = 'Styled-Select anwenden.';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_widget_styled_upload']['0']   = 'Styled-Upload';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['form_widget_styled_upload']['1']   = 'Datei-Upload Feld stylen.';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['left']                             = 'links';
+$GLOBALS['TL_LANG']['tl_bootstrap_config']['right']                            = 'rechts';
 
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['left']  = 'links';
-$GLOBALS['TL_LANG']['tl_bootstrap_config']['right'] = 'rechts';
--- module/languages/de/tl_form.php
+++ module/languages/de/tl_form.php
@@ -1,8 +1,18 @@
 <?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link http://help.transifex.com/intro/translating.html
+ * @link https://www.transifex.com/projects/p/contao-bootstrap/language/de/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2015-10-17T20:11:03+02:00
+ */
 
-$GLOBALS['TL_LANG']['tl_form']['tableless'][0] = 'Kein horizontales Formular';
-$GLOBALS['TL_LANG']['tl_form']['tableless'][1] = 'Das Formular nicht als horizontales Formular (Bootstrap) / HTML-Tabellen (Contao) darstellen.';
 
-$GLOBALS['TL_LANG']['tl_form']['bootstrap_legend']     = 'Bootstrap';
-$GLOBALS['TL_LANG']['tl_form']['bootstrap_configs'][0] = 'Bootstrap configs';
-$GLOBALS['TL_LANG']['tl_form']['bootstrap_configs'][1] = 'Choose configs which are used for this form locally.';
+$GLOBALS['TL_LANG']['tl_form']['bootstrap_configs']['0'] = 'Bootstrap configs';
+$GLOBALS['TL_LANG']['tl_form']['bootstrap_configs']['1'] = 'Choose configs which are used for this form locally.';
+$GLOBALS['TL_LANG']['tl_form']['bootstrap_legend']       = 'Bootstrap';
+
