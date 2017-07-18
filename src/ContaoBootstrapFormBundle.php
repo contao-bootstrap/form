86f02f1c816ae82c375427c6b8d62b8021bf22f3
David Molineus <david.molineus@netzmacht.de>
2017-07-18 13:22:18 +0200

    Provide bundle structure.

---
+++ src/ContaoBootstrapFormBundle.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @package    contao-bootstrap
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2017 netzmacht David Molineus. All rights reserved.
+ * @filesource
+ *
+ */
+
+namespace ContaoBootstrap\Form;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class ContaoBootstrapFormBundle.
+ *
+ * @package ContaoBootstrap\Form
+ */
+class ContaoBootstrapFormBundle extends Bundle
+{
+
+}
