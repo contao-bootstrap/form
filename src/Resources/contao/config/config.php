5d3dcad97de03f1a682179620f41f72dff246cf4
David Molineus <david.molineus@netzmacht.de>
2017-07-19 09:22:07 +0200

    Remove leftover code.

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -6,5 +6,3 @@
  * @license   LGPL 3+
  * @copyright 2013-2015 netzmacht creative David Molineus
  */
-
-//$GLOBALS['TL_HOOKS']['loadFormField'][] = array('Netzmacht\Bootstrap\Form\Contao\Hooks', 'loadFormField');
