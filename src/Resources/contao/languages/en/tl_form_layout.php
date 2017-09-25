8103bb3b642793d7d2afa147cd9e34a0c6c39269
David Molineus <david.molineus@netzmacht.de>
2017-09-25 09:45:28 +0200

    Add translation labels.

---
+++ src/Resources/contao/languages/en/tl_form_layout.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Contao Bootstrap form.
+ *
+ * @package    contao-bootstrap
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2017 netzmacht David Molineus. All rights reserved.
+ * @license    LGPL 3.0
+ * @filesource
+ */
+
+$GLOBALS['TL_LANG']['tl_form_layout']['types']['bs_default']    = 'Bootstrap default';
+$GLOBALS['TL_LANG']['tl_form_layout']['types']['bs_horizontal'] = 'Bootstrap horizontal';
