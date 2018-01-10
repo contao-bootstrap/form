daa83b6bc392a54af2e0d5a3e424a5feab2e7727
David Molineus <david.molineus@netzmacht.de>
2018-01-10 14:25:30 +0100

    Add missing author name.

--- src/Listener/FormFieldDcaListener.php
+++ src/Listener/FormFieldDcaListener.php
@@ -5,7 +5,8 @@
  *
  * @package    contao-bootstrap
  * @author     David Molineus <david.molineus@netzmacht.de>
- * @copyright  2017 netzmacht David Molineus. All rights reserved.
+ * @author     Nikolas Runde <mail@nrun.de>
+ * @copyright  2017-2018 netzmacht David Molineus. All rights reserved.
  * @license    LGPL 3.0
  * @filesource
  */
