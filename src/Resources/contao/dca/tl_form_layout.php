9352afb5727a03bf17011df99d43a5f1046352cc
David Molineus <david.molineus@netzmacht.de>
2018-05-28 14:02:59 +0200

    Bugfix: Add missing grid column classes.

--- src/Resources/contao/dca/tl_form_layout.php
+++ src/Resources/contao/dca/tl_form_layout.php
@@ -12,4 +12,39 @@
 
 $GLOBALS['TL_DCA']['tl_form_layout']['metapalettes']['bs_default extends standard'] = [];
 
-$GLOBALS['TL_DCA']['tl_form_layout']['metapalettes']['bs_horizontal extends standard'] = [];
+$GLOBALS['TL_DCA']['tl_form_layout']['metapalettes']['bs_horizontal extends standard'] = [
+    'bs_grid before widgets' => ['bs_label', 'bs_control', 'bs_offset']
+];
+
+$GLOBALS['TL_DCA']['tl_form_layout']['fields']['bs_label'] = [
+    'label'     => &$GLOBALS['TL_LANG']['tl_form_layout']['bs_label'],
+    'exclude'   => true,
+    'inputType' => 'text',
+    'eval'      => array(
+        'tl_class'       => 'w50',
+        'maxlength'      => 64,
+    ),
+    'sql'       => "varchar(64) NOT NULL default ''"
+];
+
+$GLOBALS['TL_DCA']['tl_form_layout']['fields']['bs_control'] = [
+    'label'     => &$GLOBALS['TL_LANG']['tl_form_layout']['bs_control'],
+    'exclude'   => true,
+    'inputType' => 'text',
+    'eval'      => array(
+        'tl_class'       => 'w50',
+        'maxlength'      => 64,
+    ),
+    'sql'       => "varchar(64) NOT NULL default ''"
+];
+
+$GLOBALS['TL_DCA']['tl_form_layout']['fields']['bs_offset'] = [
+    'label'     => &$GLOBALS['TL_LANG']['tl_form_layout']['bs_offset'],
+    'exclude'   => true,
+    'inputType' => 'text',
+    'eval'      => array(
+        'tl_class'       => 'w50',
+        'maxlength'      => 64,
+    ),
+    'sql'       => "varchar(64) NOT NULL default ''"
+];
