f3e066c6065e8c168ab80cd8d06d70f84c5cf2c2
David Molineus <david.molineus@netzmacht.de>
2018-05-28 14:12:37 +0200

    Update translations.

---
+++ src/Resources/contao/languages/de/tl_form_layout.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Translations are managed using Transifex. To create a new translation
+ * or to help to maintain an existing one, please register at transifex.com.
+ *
+ * @link https://www.transifex.com/signup/
+ * @link https://www.transifex.com/projects/p/contao-bootstrap/language/de/
+ *
+ * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ *
+ * last-updated: 2018-05-28T11:57:15+00:00
+ */
+
+
+$GLOBALS['TL_LANG']['tl_form_layout']['types']['bs_default']    = 'Bootstrap Standard (vertikal)';
+$GLOBALS['TL_LANG']['tl_form_layout']['types']['bs_horizontal'] = 'Bootstrap Horizontal';
+
