3ddb0a5cf57f198028ef1bd6300a3aee312df342
David Molineus <david.molineus@netzmacht.de>
2022-10-03 10:07:11 +0200

    Refactor configuration

--- src/ContaoBootstrapFormBundle.php
+++ src/ContaoBootstrapFormBundle.php
@@ -4,8 +4,18 @@ declare(strict_types=1);
 
 namespace ContaoBootstrap\Form;
 
+use ContaoBootstrap\Core\DependencyInjection\ContaoBootstrapCoreExtension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+use function assert;
 
 class ContaoBootstrapFormBundle extends Bundle
 {
+    public function build(ContainerBuilder $container): void
+    {
+        $extension = $container->getExtension('contao_bootstrap');
+        assert($extension instanceof ContaoBootstrapCoreExtension);
+        $extension->addComponent(new ContaoBootstrapComponent());
+    }
 }
--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -8,11 +8,13 @@ use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Contao\ManagerPlugin\Config\ConfigPluginInterface;
 use ContaoBootstrap\Core\ContaoBootstrapCoreBundle;
 use ContaoBootstrap\Form\ContaoBootstrapFormBundle;
 use Netzmacht\Contao\FormDesigner\NetzmachtContaoFormDesignerBundle;
+use Symfony\Component\Config\Loader\LoaderInterface;
 
-class Plugin implements BundlePluginInterface
+final class Plugin implements BundlePluginInterface, ConfigPluginInterface
 {
     /**
      * {@inheritDoc}
@@ -29,5 +31,13 @@ class Plugin implements BundlePluginInterface
                     ]
                 ),
         ];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function registerContainerConfiguration(LoaderInterface $loader, array $managerConfig): void
+    {
+        $loader->load(__DIR__ . '/../Resources/config/contao_bootstrap.yaml');
     }
 }
