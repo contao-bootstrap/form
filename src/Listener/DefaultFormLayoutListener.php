c915ae1710293b9c484a578e9f32878a4e1c8a36
David Molineus <david.molineus@netzmacht.de>
2026-06-02 16:28:00 +0200

    Refactor getPageLayout hook to use #[AsHook] attribute and remove legacy configuration

--- src/Listener/DefaultFormLayoutListener.php
+++ src/Listener/DefaultFormLayoutListener.php
@@ -4,20 +4,14 @@ declare(strict_types=1);
 
 namespace ContaoBootstrap\Form\Listener;
 
+use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;
 use Contao\LayoutModel;
 use Contao\PageModel;
 use Netzmacht\Contao\FormDesigner\Listener\AbstractListener;
 
 final class DefaultFormLayoutListener extends AbstractListener
 {
-    /**
-     * Create default bootstrap form layout.
-     *
-     * @param PageModel   $pageModel   Page model.
-     * @param LayoutModel $layoutModel Layout model.
-     *
-     * @SuppressWarnings(PHPMD.UnusedFormalParameter)
-     */
+    #[AsHook('getPageLayout')]
     public function onGetPageLayout(PageModel $pageModel, LayoutModel $layoutModel): void
     {
         if ($this->manager->hasDefaultThemeLayout()) {
--- src/Resources/contao/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-$GLOBALS['TL_HOOKS']['getPageLayout'][] = [
-    'contao_bootstrap.form.listener.default_form_layout',
-    'onGetPageLayout',
-];
