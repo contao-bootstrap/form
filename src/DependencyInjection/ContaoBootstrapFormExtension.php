173fbcb14c52c2186823c885f1c41ba96e3b9294
David Molineus <david.molineus@netzmacht.de>
2026-06-02 16:07:16 +0200

    Fix parent class of extension

--- src/DependencyInjection/ContaoBootstrapFormExtension.php
+++ src/DependencyInjection/ContaoBootstrapFormExtension.php
@@ -7,8 +7,8 @@ namespace ContaoBootstrap\Form\DependencyInjection;
 use Override;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 final class ContaoBootstrapFormExtension extends Extension
 {
